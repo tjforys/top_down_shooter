5b0f1e7efd41e4dda716319e6e4f8a4af0fa066a
t <tomekk925@gmail.com>
2024-03-28 19:49:28 +0100

    added player collision

--- objects/player.py
+++ objects/player.py
@@ -8,14 +8,14 @@ class Player():
         self._speed = speed
 
     
-    def move(self):
+    def move(self, area_x, area_y):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] and self.position[0] > 0:
             self.position[0] -= self._speed
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] and self.position[1] > 0:
             self.position[1] -= self._speed
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] and self.position[1] < area_y:
             self.position[1] += self._speed
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] and self.position[0] < area_x:
             self.position[0] += self._speed
             
