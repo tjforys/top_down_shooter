4d472629f6120899ed30c5a6bd548c36a0ca1dd7
Xaxedd <sebastiankostr@gmail.com>
2024-03-30 18:44:35 +0100

    changed paths in FilePaths to relative and deleted consts folder

--- classes/file_paths.py
+++ classes/file_paths.py
@@ -1,10 +1,7 @@
-from consts.user_consts import Consts
-
-
 class FilePaths:
-    gif_monday_1 = fr"{Consts.folder_path}\sprite\monday"
-    gif_monday_2 = fr"{Consts.folder_path}\sprite\monday_2"
+    gif_monday_1 = fr"sprite\monday"
+    gif_monday_2 = fr"sprite\monday_2"
 
-    png_amogus = fr"{Consts.folder_path}\sprite\amogus.png"
+    png_amogus = fr"sprite\amogus.png"
 
-    mp3_monday = fr"{Consts.folder_path}\mp3\monday.mp3"
+    mp3_monday = fr"mp3\monday.mp3"
--- consts/README.txt
+++
@@ -1,4 +0,0 @@
-Need to create file "user_conts" with following structure:
-
-class Consts:
-    folder_path = r"xxx"
