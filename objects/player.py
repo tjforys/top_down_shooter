6f4cf4bddf3126d91fbc5b880ea9f359d5915dd6
tjforys <126683031+tjforys@users.noreply.github.com>
2024-03-30 02:28:49 +0100

    [3] Refactors (#1)
    * added player collision
    
    * refactored speed calculations
    
    * deleted unused comment
    
    * [2] added player sprites, added player sprite rotating, refactored some code (#3)
    
    * added player sprites, added player sprite rotating, refactored some code
    
    * [1] added gif in the background of the game (#2)
    
    * added gif in the background of the game
    
    * made gif_background class and added function to draw background gif to Draw
    
    * added bg sound, fixed draw class (#4)
    
    * added bg sound, fixed draw class
    
    * added music class responsible for background music
    
    * no comment
    
    ---------
    
    Co-authored-by: t <tomekk925@gmail.com>
    Co-authored-by: tjforys <126683031+tjforys@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: t <tomekk925@gmail.com>
    Co-authored-by: tjforys <126683031+tjforys@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Xaxedd <86934302+Xaxedd@users.noreply.github.com>

--- objects/player.py
+++ objects/player.py
@@ -1,21 +1,30 @@
 import pygame
 
+from classes.direction_enums import Directions
+
 
 class Player():
-    def __init__(self, position: list, radius: int, speed: float):
+    def __init__(self, sprite, position: list, radius: int, speed: float):
+        self.sprite = sprite
+        self.rotation = Directions.RIGHT
         self.position = position
         self.radius = radius
         self._speed = speed
 
     
-    def move(self):
+    def move(self, area_x, area_y):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] and self.position[0] > 0:
             self.position[0] -= self._speed
-        if keys[pygame.K_w]:
+            self.rotation = Directions.LEFT
+
+        if keys[pygame.K_w] and self.position[1] > 0:
             self.position[1] -= self._speed
-        if keys[pygame.K_s]:
+
+        if keys[pygame.K_s] and self.position[1] < area_y:
             self.position[1] += self._speed
-        if keys[pygame.K_d]:
+            
+        if keys[pygame.K_d] and self.position[0] < area_x:
             self.position[0] += self._speed
+            self.rotation = Directions.RIGHT
             
---
+++ objects/screen.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Screen():
+    def __init__(self, screen_x, screen_y):
+        self.x = screen_x
+        self.y = screen_y
+        self.screen = self._initialize_screen()
+        self.font = pygame.font.SysFont('Comic Sans MS', 30)
+
+    
+    def _initialize_screen(self):
+        return pygame.display.set_mode([self.x, self.y])
+    
+
+    def show_current_time(self, current_time_in_ms: int) -> None:
+        text_surface = self.font.render(f'{current_time_in_ms/1000}s', False, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(self.x/2, 15))
+        self.screen.blit(text_surface, text_rect)
+
+    
+    def fill_screen(self, color: tuple):
+        self.screen.fill(color)    
