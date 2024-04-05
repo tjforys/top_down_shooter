34862e213c5588dc5388a4d76bca72615c648049
Xaxedd <sebastiankostr@gmail.com>
2024-04-05 15:15:27 +0200

    added enter

--- objects/player.py
+++ objects/player.py
@@ -28,6 +28,7 @@ class Player:
         if keys[pygame.K_d]:
             self.position[0] += self._speed
             self.rotation = Directions.RIGHT
+
         self.position = Movement.put_back_in_arena_if_outside(area_x, area_y, self.position)
 
 
@@ -41,5 +42,6 @@ class Player:
             self.position[0] += dash_distance
         if keys[pygame.K_s]:
             self.position[1] += dash_distance
+
         self.position = Movement.put_back_in_arena_if_outside(area_x, area_y, self.position)
             
