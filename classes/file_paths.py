20f036ff374e8d15e1381dbde3021c5144238bc0
Xaxedd <sebastiankostr@gmail.com>
2024-05-04 19:18:43 +0200

    changed how shooting works, added ar15, added shootType enum

--- classes/file_paths.py
+++ classes/file_paths.py
@@ -2,12 +2,15 @@ class FilePaths:
     gif_monday_1 = r"sprite\monday"
     gif_monday_2 = r"sprite\monday_2"
 
-    png_enemy_sprite_black_impostor = r"sprite/enemy.png"
     png_amogus = r"sprite\amogus.png"
+
+    png_enemy_sprite_black_impostor = r"sprite/enemy.png"
     png_goku = "sprite/goku.png"
+    png_school_shooter = "sprite/school_shooter.png"
+
     png_glock_cursor = r"sprite\amogus_cursor.png"
     png_shotgun_cursor = "sprite/shotgun_cursor.png"
-    png_school_shooter = "sprite/school_shooter.png"
+    png_ar15_cursor = "sprite/ar15_cursor.png"
 
     mp3_monday = r"mp3\monday.mp3"
     mp3_amongus = r"mp3\amongus.mp3"
